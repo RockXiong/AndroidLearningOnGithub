3c56d8f90940a9208d5bf026b6477e40bf591aba
xb <976625906axb@gmail.com>
2017-02-04 17:43:12 +0800

    create ViewEvent module

---
+++ viewevent/src/main/java/com/xb/viewevent/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xb.viewevent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
